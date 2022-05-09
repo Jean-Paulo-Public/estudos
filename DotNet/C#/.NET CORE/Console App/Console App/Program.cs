2cbc1d9c4d7e2337c4bc4642b6aafbe0cbdc435b
Jean Paulo <jeanpauloriopreto@gmail.com>
2022-05-08 22:14:48 -0300

    DotNet

---
+++ DotNet/C#/.NET CORE/Console App/Console App/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+//using System.Collections.Generic;
+//using System.Linq;
+using System.Text;
+//using System.Threading.Tasks;
+
+namespace Console_App
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("teste");
+            string EntradaUsuário = Console.ReadLine();
+            Console.WriteLine(EntradaUsuário);
+            teste(Console.ReadLine());
+            Console.ReadLine();
+        }
+
+        static void teste(string teste) { 
+            Console.WriteLine("Teste"+teste); 
+        }
+    }
+}
